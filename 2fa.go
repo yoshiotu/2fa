3843772f87c5c7adb0a0cb64deca765226d09efe
Yoshio Turner <yoshio@banyanops.com>
2017-09-15 22:14:48 +0000

    minor improvements

--- 2fa.go
+++ 2fa.go
@@ -11,8 +11,8 @@ import (
 	"io/ioutil"
 	"net/url"
 
-	qr "github.com/mdp/rsc/qr" // updated to a valid path
 	dgoogauth "github.com/dgryski/dgoogauth"
+	qr "github.com/rsc/qr"
 )
 
 const (
@@ -34,19 +34,20 @@ func main() {
 	secretBase32 := base32.StdEncoding.EncodeToString(secret)
 
 	account := "user@example.com"
+	issuer := "NameOfMyService"
 
 	URL, err := url.Parse("otpauth://totp")
 	if err != nil {
 		panic(err)
 	}
 
-	URL.Path += "/NameOfMyService:" + account
+	URL.Path += "/" + url.PathEscape(issuer) + ":" + url.PathEscape(account)
+
 	params := url.Values{}
 	params.Add("secret", secretBase32)
-	params.Add("issuer", "NameOfMyService")
+	params.Add("issuer", issuer)
 
 	URL.RawQuery = params.Encode()
-
 	fmt.Printf("URL is %s\n", URL.String())
 
 	code, err := qr.Encode(URL.String(), qr.Q)
@@ -61,25 +62,36 @@ func main() {
 
 	fmt.Printf("QR code is in %s. Please scan it into Google Authenticator app.\n", qrFilename)
 
-	var token string
-	fmt.Printf("Please enter the token value: ")
-	fmt.Scanln(&token)
-
+	// The OTPConfig gets modified by otpc.Authenticate() to prevent passcode replay, etc.,
+	// so allocate it once and reuse it for multiple calls.
 	otpc := &dgoogauth.OTPConfig{
 		Secret:      secretBase32,
 		WindowSize:  3,
 		HotpCounter: 0,
+		// UTC:         true,
 	}
 
-	val, err := otpc.Authenticate(token)
-	if err != nil {
-		panic(err)
-	}
+	for {
+		var token string
+		fmt.Printf("Please enter the token value (or q to quit): ")
+		fmt.Scanln(&token)
 
-	if val {
-		fmt.Printf("Got true val\n")
-	} else {
-		fmt.Printf("Got false val\n")
+		if token == "q" {
+			break
+		}
+
+		val, err := otpc.Authenticate(token)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if !val {
+			fmt.Println("Sorry, Not Authenticated")
+			continue
+		}
+
+		fmt.Println("Authenticated!")
 	}
 	return
 }
